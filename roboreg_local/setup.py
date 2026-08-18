5c4869ba175758c4efca6eec6ba4adae0ffa4850
mhubii <m.huber_1994@hotmail.de>
2026-08-18 13:20:50 +0100

    prepare release cycle switch (#66)

--- roboreg_local/setup.py
+++ roboreg_local/setup.py
@@ -6,7 +6,7 @@ package_name = "roboreg_local"
 
 setup(
     name=package_name,
-    version="0.2.0",
+    version="0.3.0",
     packages=find_packages(exclude=["test"]),
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
